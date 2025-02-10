f814d926de737291fcba10a2c2df9a71d67de878
JEONGJAEIK <wodlr1207@naver.com>
2025-02-10 12:19:08 +0900

    .

--- backend/src/main/java/com/project/backend/domain/book/controller/BookController.java
+++ backend/src/main/java/com/project/backend/domain/book/controller/BookController.java
@@ -2,6 +2,7 @@ package com.project.backend.domain.book.controller;
 
 import com.project.backend.domain.book.dto.BookDTO;
 import com.project.backend.domain.book.service.BookService;
+import com.project.backend.global.authority.CustomUserDetails;
 import com.project.backend.global.response.GenericResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
